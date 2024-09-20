c3436bf6ace4c1e66117b9c429e2b86aa17084cd
Teddysir <sdzx0719@naver.com>
2024-09-20 15:31:07 +0900

    git remote

---
+++ src/main/java/gamza/project/backstudy/BackStudyApplication.java
@@ -0,0 +1,13 @@
+package gamza.project.backstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gamza/project/backstudy/BackStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package gamza.project.backstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
