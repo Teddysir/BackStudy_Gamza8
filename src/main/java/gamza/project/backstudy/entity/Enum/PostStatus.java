8cf55ac7617a33e10a66438a69781ffbdd7acc13
Teddysir <sdzx0719@naver.com>
2024-09-20 15:51:53 +0900

    add(entity): added PostEntity

---
+++ src/main/java/gamza/project/backstudy/entity/Enum/PostStatus.java
@@ -0,0 +1,6 @@
+package gamza.project.backstudy.entity.Enum;
+
+public enum PostStatus {
+    REGISTERED,
+    ABANDONED
+}
---
+++ src/main/java/gamza/project/backstudy/entity/PostEntity.java
@@ -0,0 +1,33 @@
+package gamza.project.backstudy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String status;
+
+
+}
